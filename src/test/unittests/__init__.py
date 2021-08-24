92643796219d9d763c926cfcc5560bf74342553c
db developer <db.developer@gmx.de>
2021-08-25 00:38:54 +0200

    extended testing

--- src/test/unittests/__init__.py
+++ src/test/unittests/__init__.py
@@ -6,6 +6,12 @@
 #   implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
 """ tests for package 'org.slashlib.py.eos.core.inspect' """
-from __future__     import absolute_import
-
-from .inspect       import Test_Module_Inspect
+from .utils_get_local_part          import Test_Module_utils_get_local_part
+from .inspect_iscallable            import Test_Module_inspect_iscallable
+from .inspect_isclass               import Test_Module_inspect_isclass
+from .inspect_isfunction            import Test_Module_inspect_isfunction
+from .inspect_ismethod              import Test_Module_inspect_ismethod
+from .inspect_ismethodorfunction    import Test_Module_inspect_ismethodorfunction
+from .inspect_isstaticmethod        import Test_Module_inspect_isstaticmethod
+from .inspect_isclassmethod         import Test_Module_inspect_isclassmethod
+from .inspect                       import Test_Library_Exports
